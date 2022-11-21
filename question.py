c8674b2879d75cebf5e0ff45809b164113fe549e
sonyjain09 <sonam09jain@yahoo.com>
2022-11-21 10:04:37 -0600

    handling faulty user input

--- question.py
+++ question.py
@@ -53,7 +53,7 @@ def createList(r1, r2):
 
 available_questions = {
     "NS": createList(0, 49), #50 natural science questions on medium difficulty
-    "NSE": createList(0,19), #20 
+    "NSE": createList(0,19), #20 natural science questions on easy difficulty
     "VG": createList(0, 49), #50 video game questions on medium pools with TV
     "TV": createList(0,49), #50 TV questions on TV pools with vg
     "G": createList(0, 24), #25 geography questions pools with H, he
@@ -64,7 +64,7 @@ available_questions = {
 }
 
 
-def cleanUnicode(question, options)
+def cleanUnicode(question, options):
     for i in range(4):
         options[i] = html.unescape(options[i])
     question = html.unescape(question)
@@ -89,24 +89,27 @@ def getQuestion(category, category_name):
 
     player_answer = input("Type A,B,C,D for your corresponding answer\n")
     player_answer = player_answer.lower()
-
-    match player_answer:
-        case "a":
-            player_answer_index = 0
-        case "b":
-            player_answer_index = 1
-        case "c":
-            player_answer_index = 2
-        case "d":
-            player_answer_index = 3
-        case "1":
-            player_answer_index = 0
-        case "2":
-            player_answer_index = 1
-        case "3":
-            player_answer_index = 2
-        case "4":
-            player_answer_index = 3
+    player_answer_index = None
+    while player_answer_index == None:
+        match player_answer:
+            case "a":
+                player_answer_index = 0
+            case "b":
+                player_answer_index = 1
+            case "c":
+                player_answer_index = 2
+            case "d":
+                player_answer_index = 3
+            case "1":
+                player_answer_index = 0
+            case "2":
+                player_answer_index = 1
+            case "3":
+                player_answer_index = 2
+            case "4":
+                player_answer_index = 3
+            case other:
+                player_answer = input("Invalid input. Type A,B,C,D or 1,2,3,4 for your corresponding answer\n")
 
     print("\n")
 
@@ -128,43 +131,48 @@ while game:
     ind = input(
         "What category are you in?\n1) Natural Science\n2) Entertainment\n3) History and Geography\n4) General Knowledge\n"
     )
+    category = None
+    category_name = None
     horg = random.randint(0, 1)
-    match ind:
-        case "1":
-            match horg:
-                case 0:
-                    category = NS
-                    category_name = "NS"
-                case 1:
-                    category = NSE
-                    category_name = "NSE"
-        case "2":
-            match horg:
-                case 0:
-                    category = VG
-                    category_name = "VG"
-                case 1:
-                    category = TV
-                    category_name = "TV"
-        case "3":
-            horg = random.randint(0, 2)
-            match horg:
-                case 0:
-                    category = H
-                    category_name = "H"
-                case 1:
-                    category = G
-                    category_name = "G"
-                case 2:
-                    category = HE
-                    category_name = "HE"
-        case "4":
-            match horg:
-                case 0:
-                    category = GK
-                    category_name = "GK"
-                case 1:
-                    category = GKE
-                    category_name = "GKE"
+    while category == None and category_name == None:
+        match ind:
+            case "1":
+                match horg:
+                    case 0:
+                        category = NS
+                        category_name = "NS"
+                    case 1:
+                        category = NSE
+                        category_name = "NSE"
+            case "2":
+                match horg:
+                    case 0:
+                        category = VG
+                        category_name = "VG"
+                    case 1:
+                        category = TV
+                        category_name = "TV"
+            case "3":
+                horg = random.randint(0, 2)
+                match horg:
+                    case 0:
+                        category = H
+                        category_name = "H"
+                    case 1:
+                        category = G
+                        category_name = "G"
+                    case 2:
+                        category = HE
+                        category_name = "HE"
+            case "4":
+                match horg:
+                    case 0:
+                        category = GK
+                        category_name = "GK"
+                    case 1:
+                        category = GKE
+                        category_name = "GKE"
+            case other:
+                ind = input("Invalid input. What category are you in?\n1) Natural Science\n2) Entertainment\n3) History and Geography\n4) General Knowledge\n")
 
     getQuestion(category, category_name)
